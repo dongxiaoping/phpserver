c1c824063530801eedfb3d3b247b801b4a94b7c1
dxp <dongxiaoping>
2020-01-19 22:37:03 +0800

    优化

--- application/extra/roomGameConfig.php
+++ application/extra/roomGameConfig.php
@@ -2,6 +2,6 @@
 return [
     'dealAction' => 11, //摇色子以及发牌行为耗时
     'betTime' => 15, //下注持续时间 s
-    'showDownTime' => 18, //开牌 大小对比 结果显示
+    'showDownTime' => 17, //开牌 大小对比 结果显示
     'landlordLastCount' => 5 //每一次当庄场次数
 ];
