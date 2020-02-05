70a219389ae3716d5b87ab8367bde96692644466
dxp <dongxiaoping>
2020-02-05 23:24:30 +0800

    优化配置

--- application/extra/roomGameConfig.php
+++ application/extra/roomGameConfig.php
@@ -7,5 +7,6 @@ return [
     'landlordLastCount' => 5, //每一次当庄场次数 s
     'roomTimeoutTime' => 3600, //socket房间存在超时时间，主要产生原因是抢庄以及开始时间不确定s
     'invalidRoomCheckTime' => 600, //无效房间检查周期 s,
-    'diamondPrice' => 0.5 //每个钻的单价 单位：元
+    'diamondPrice' => 0.5, //每个钻的单价 单位：元
+    'chipValList' => [1, 2, 5, 10] //下注值集合
 ];
