c319db7b92fc4af64850ee2492e50b7538523e84
dxp <dongxiaoping>
2020-03-24 10:04:54 +0800

    测试

--- application/race/model/RoomOP.php
+++ application/race/model/RoomOP.php
@@ -43,7 +43,7 @@ class RoomOP
     {
         $table = new Room();
         $list = $table->where("creatUserId", $id)->where("roomState", "<=", 2)
-            ->order('roomState desc,creatTime')->select();
+            ->order('roomState desc,creatTime desc')->select();
         return $list;
     }
 
