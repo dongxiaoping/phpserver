43ea957ec95bf0cd7283a8b773511232dc2bae91
dxp <dongxiaoping>
2020-05-01 15:10:32 +0800

    优化配置

--- application/extra/createDiamondConfig.php
+++ application/extra/createDiamondConfig.php
@@ -1,6 +1,18 @@
 <?php
 return array(
-    'roomPeople' => array('one' => array('peoplecount' => 6), 'two' => array('peoplecount' => 8), 'three' => array('peoplecount' => 10)),
-    'totalRace' => array('one' => array('raceCount' => 10, 'aaDiamond' => 3, 'daiKaiDiamond' => 20), 'two' => array('raceCount' => 20, 'aaDiamond' => 3, 'daiKaiDiamond' => 25), 'three' => array('raceCount' => 25, 'aaDiamond' => 4, 'daiKaiDiamond' => 30)),
-    'betLimit' => array('one' => array('limitVal' => 10, 'aaRate' => 1, 'daiKaiRate' => 1), 'two' => array('limitVal' => 100, 'aaRate' => 1.5, 'daiKaiRate' => 1.5), 'three' => array('limitVal' => 200, 'aaRate' => 2, 'daiKaiRate' => 2)),
+    'roomPeople' => array(
+        'one' => array('peoplecount' => 6),
+        'two' => array('peoplecount' => 8),
+        'three' => array('peoplecount' => 10)),
+    'totalRace' => array(
+        'one' => array('raceCount' => 12, 'aaDiamond' => 3, 'daiKaiDiamond' => 20),
+        'two' => array('raceCount' => 16, 'aaDiamond' => 3, 'daiKaiDiamond' => 26),
+        'three' => array('raceCount' => 20, 'aaDiamond' => 4, 'daiKaiDiamond' => 32),
+        'four' => array('raceCount' => 24, 'aaDiamond' => 4, 'daiKaiDiamond' => 38)),
+    'betLimit' => array(
+        'one' => array('limitVal' => 50, 'aaRate' => 1, 'daiKaiRate' => 1),
+        'two' => array('limitVal' => 100, 'aaRate' => 1, 'daiKaiRate' => 1),
+        'three' => array('limitVal' => 200, 'aaRate' => 1, 'daiKaiRate' => 1),
+        'four' => array('limitVal' => 300, 'aaRate' => 2, 'daiKaiRate' => 2),
+        ),
 );
