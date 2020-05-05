1724850ee8ad5c8a63b98205fdeb07ebc5f5ac04
dxp <dongxiaoping>
2020-05-05 16:34:58 +0800

    添加语音接口

--- application/extra/roomGameConfig.php
+++ application/extra/roomGameConfig.php
@@ -8,5 +8,6 @@ return [
     'roomTimeoutTime' => 3600, //socket房间存在超时时间，主要产生原因是抢庄以及开始时间不确定s
     'diamondPrice' => 0.5, //每个钻的单价 单位：元
     'chipValList' => [5, 10, 50, 100], //下注值集合
+    'userAudioUrl'=>'https://www.toplaygame.cn/phpserver/public/voice/',//用户图标地址
     'userIconUrl'=>'https://www.toplaygame.cn/phpserver/public/upload/'//用户图标地址
 ];
---
+++ application/race/controller/Voice.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace app\race\controller;
+use think\Log;
+use think\response\Json;
+
+class Voice
+{
+    //http://127.0.0.1/phpserver/public/index.php/race/voice/uploadVoice
+    public function uploadVoice()
+    {
+        header("Access-Control-Allow-Origin: *");
+        try{
+            $info =  file_get_contents('php://input');
+            $list = json_decode($info,true);
+            $file = $list["file"];
+            $name = $this->loadVoiceFile($file);
+            if($name == null){
+                echo getJsonStringByParam(0, "上传异常", "");
+            }else{
+                echo getJsonStringByParam(1, "上传成功", $name);
+            }
+        } catch (Exception $e) {
+            echo getJsonStringByParam(0, "上传失败", "");
+        }
+    }
+
+    private function loadVoiceFile($baseData){
+        try {
+            $up_dir = './voice/';
+            if (!file_exists($up_dir)) {
+                mkdir($up_dir, 0777);
+            }
+            if (preg_match('/^(data:\s*audio\/(\w+);base64,)/', $baseData, $result)) {
+                $type = $result[2];
+                if (in_array($type, array('mp3'))) {
+                    $picName = date('YmdHis') . '.' . $type;
+                    $path = $up_dir . $picName;
+                    if (file_put_contents($path, base64_decode(str_replace($result[1], '', $baseData)))) {
+                        $img_path = str_replace('../../..', '', $path);
+                        return $picName;
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        } catch (Exception $e) {
+            return null;
+        }
+
+    }
+}
