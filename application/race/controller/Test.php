098c2dca1d85ba9a14b83fee6501c53328b28368
dxp <dongxiaoping>
2020-12-29 16:11:45 +0800

    修改配置

--- application/race/controller/Test.php
+++ application/race/controller/Test.php
@@ -8,7 +8,7 @@ use app\race\service\CostServer;
 use app\race\service\RechargeServer;
 use app\race\service\socket\SocketServer;
 use think\Log;
-
+use Grafika\Grafika;
 class Test
 {
     public function __construct()
@@ -19,31 +19,11 @@ class Test
 //http://127.0.0.1/phpserver/public/index.php/race/test/test
     public function test()
     {
-        echo 2;
-//        header("Access-Control-Allow-Origin: *");
-//        $up_dir = './upload/';//存放在当前目录的upload文件夹下
-//        $baseData = trim($_POST['file']);
-//        if(!file_exists($up_dir)){
-//            mkdir($up_dir,0777);
-//        }
-//        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $baseData, $result)) {
-//            $type = $result[2];
-//            if(in_array($type,array('pjpeg','jpeg','jpg','gif','bmp','png'))){
-//                $new_file = $up_dir.date('YmdHis_').'.'.$type;
-//                if(file_put_contents($new_file, base64_decode(str_replace($result[1], '', $baseData)))){
-//                    $img_path = str_replace('../../..', '', $new_file);
-//                    echo '图片上传成功</br>![](' .$img_path. ')';
-//                }else{
-//                    echo '图片上传失败</br>';
-//
-//                }
-//                echo '文件正确';
-//            }else{
-//                echo '文件错误';
-//            }
-//            echo $baseData;
-//        }else{
-//           echo '数据异常';
-//        }
+        $editor = Grafika::createEditor();
+        $up_dir = './upload/20201229152624.jpeg';
+        $editor->open( $image, $up_dir );
+        $editor->resizeExact( $image, 200, 200 );
+        $editor->save( $image, './upload/33333.jpeg', null, 90 );
+
     }
 }
