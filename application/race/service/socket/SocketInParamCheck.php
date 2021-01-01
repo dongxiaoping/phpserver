29ecfc0aabd08d96fa1b332f3098c03533c571f6
Administrator <466476120@qq.com>
2021-01-01 18:41:57 +0800

    fix

--- application/race/service/socket/SocketInParamCheck.php
+++ application/race/service/socket/SocketInParamCheck.php
@@ -17,42 +17,52 @@ class SocketInParamCheck
                 if (isset($data['info']['roomId']) && isset($data['info']['userId'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$START_GAME_REQ: //
                 if (isset($data['info']['roomId']) && isset($data['info']['userId'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$LANDLORD_SELECTED_REQ: //
                 if (isset($data['info']['roomId']) && isset($data['info']['raceNum']) && isset($data['info']['landlordId'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$RACE_BET_REQ: //
                 if (isset($data['info']['userId']) && isset($data['info']['roomId']) && isset($data['info']['raceNum'])
                     && isset($data['info']['betLocation']) && isset($data['info']['betVal'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$CANCEL_BET_REQ: //
                 if (isset($data['info']['userId']) && isset($data['info']['roomId']) && isset($data['info']['raceNum'])
                     && isset($data['info']['betLocation'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$CHAT_CARTON_MESSAGE_REQ: //
                 if (isset($data['info']['roomId']) && isset($data['info']['info'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$KICK_OUT_MEMBER_REQ: //
                 if (isset($data['info']['roomId']) && isset($data['info']['kickUserId'])) {
                     return true;
                 }
+                break;
             case SocketActionTag::$AUDIO_PLAY_NOTICE: //
                 if (isset($data['info']['roomId']) && isset($data['info']['userId'])
                     && isset($data['info']['voiceName'])) {
                     return true;
                 }
-            case SocketActionTag::$SURE_BE_LANDLORD_IN_TURN:
-                if (isset($data['info']['roomId']) && isset($data['info']['userId'])
+                break;
+            case SocketActionTag::$SURE_BE_LANDLORD_IN_TURN :
+            case SocketActionTag::$SURE_BE_LANDLORD_PASS :
+            if (isset($data['info']['roomId']) && isset($data['info']['userId'])
                     && isset($data['info']['raceNum'])) {
                     return true;
                 }
+                break;
             default:
                 return false;
         }
